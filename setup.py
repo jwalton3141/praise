5f72f14b823ed793e6522b18f0291470cbc33f52
Jack Walton (PGR) <j.walton4@newcastle.ac.uk>
2019-12-02 17:02:50 +0000

    bump version

--- setup.py
+++ setup.py
@@ -6,14 +6,12 @@ except ImportError:
 setup(
     name='scold',
     packages=['scold'],
-    version='1.0',
-    description='Scold it',
-    long_description='Like [praise](https://github.com/funtion/praise), but when you (or your colleagues)
-have been bad.',
+    version='1.1',
+    description='Hurt colleagues with words',
     author='Jack Walton',
     author_email='jwalton3141@gmail.com',
     url='https://github.com/jwalton3141/scold',
-    download_url='https://github.com/jwalton3141/scold/archive/1.0.tar.gz',
+    download_url='https://github.com/jwalton3141/scold/archive/1.1.tar.gz',
     keywords=['scold', 'logging'],
     classifiers=[],
     test_suite='nose.collector',
